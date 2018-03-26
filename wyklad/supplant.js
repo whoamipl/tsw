6c51622dfb23510fc874a30d04c3a4155e1e9728
Michał Fierek <mfierek@protonmail.ch>
2018-03-26 23:10:46 +0200

    Created and added supplant function to String

---
+++ wyklad/supplant.js
@@ -0,0 +1,9 @@
+String.prototype.supplant = function (data) {
+  let output = this;
+  Object.entries(data)
+		.forEach(([propName, propValue]) => {
+			var pattern = new RegExp('{' + propName + '}','g');
+			output = output.replace(pattern, propValue);
+		})
+    return output;
+}
