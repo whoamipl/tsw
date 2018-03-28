c287cb01e0b6ad2ed7f0d4b37038ea6423948ed1
mfierek <mfierek@protonmail.ch>
2018-03-28 14:11:24 +0000

    Added POC

--- wyklad/supplant.js
+++ wyklad/supplant.js
@@ -4,7 +4,7 @@ String.prototype.supplant = function (data) {
   let output = this;
   Object.entries(data)
 		.forEach(([propName, propValue]) => {
-			var pattern = new RegExp('{' + propName + '}','g');
+			var pattern = new RegExp('{' + propName + '}', 'g');
 			output = output.replace(pattern, propValue);
 		});
   return output;
