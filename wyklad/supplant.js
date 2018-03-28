34f55940cd82041e26efded7926ff8abd9ce6901
Michał Fierek <mfierek@protonmail.ch>
2018-03-28 13:31:17 +0200

    Working on appFun function

--- wyklad/supplant.js
+++ wyklad/supplant.js
@@ -1,9 +1,19 @@
+/* jshint strict: global, devel: true, esversion: 6 */
+'use strict';
 String.prototype.supplant = function (data) {
   let output = this;
   Object.entries(data)
 		.forEach(([propName, propValue]) => {
 			var pattern = new RegExp('{' + propName + '}','g');
 			output = output.replace(pattern, propValue);
-		})
+		});
   return output;
-}
+};
+
+var test = '<h2>{replaceThis}</h2> Some sample text {andThis}';
+var data = {
+	replaceThis: 'replaced',
+	andThis: 'this too'
+};
+
+console.log(test.supplant(data));
