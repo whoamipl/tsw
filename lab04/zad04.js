8899db1dffbe424ce4cf08559ee22b8037b7d4d7
= <mfierek@protonmail.ch>
2018-04-04 13:28:12 +0200

    Working on lab04

---
+++ lab04/zad04.js
@@ -0,0 +1,33 @@
+/*jshint strict: global */
+'use strict';
+const ocena = (code) => {
+    return (move) => {
+        let result = {
+            black: 0,
+            white: 0
+        };
+
+        if (!Array.isArray(move) || !Array.isArray(code)) {  
+            throw new Error("Niepoprawne argumenty!")
+        }
+
+        if (move.length !== code.length) {
+            throw new Error("Tablice muszą mieć taki sam rozmiar!")
+        }
+
+        let whiteArray = code
+            .filter((color,index) => { 
+                let tmp = [];
+                if (!move[index] == color) {
+                    tmp.push(move[index]);
+            }
+            return tmp;
+        });
+        result.black = code.length - whiteArray.length;
+        result.white = code.filter( function(color) { return delete this[this.indexOf(color)] }, whiteArray ).length;
+        console.log(result);
+    }
+} 
+
+var test = ocena([4,3,2,1]);
+test([1,2,3,4]);
