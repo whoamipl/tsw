122b71a505683c58e34acbf9134acd8fe4f541b3
Michał Fierek <mfierek@protonmail.ch>
2018-04-21 16:03:34 +0200

    Almost done, problem with off and bind click event after dbclick

--- cw-dom_01/skrypt_01.js
+++ cw-dom_01/skrypt_01.js
@@ -1,21 +1,38 @@
 //jshint browser: true, esversion:6
 $(() => {
-    $('tr').on('click', (e) => {
+    function addYellowClass(e) {
         $('tr').removeClass('yellow');
         $(e.target).parent().addClass('yellow');
+    }
+    $('tr').on('click', addYellowClass);
+    $(document).keydown((e) => {
+        if (38 === e.which) {
+            $('.yellow').after(function() { 
+               return this.previousElementSibling;
+            });
+        }
+
+        if (40 === e.which) {
+            $('.yellow').before(function () {
+                return this.nextElementSibling;
+            });
+        }
     });
-    
     $(document).on('click', (e) => {
         if (!$('td').is(e.target)) {
             $('tr').removeClass('yellow');
         }
     });
-// replace innerHTML of td with input
+
     $('td').dblclick((e) => {
+        console.log($('tr').off('click'));
         let cell = $(e.target);
-        cell.('<input type=text value=\"'+ cell.text() +'\"/>');
+        cell.wrapInner('<input type=text value=\"'+ cell.text() +'\"/>');
         $(document).keydown((e) => {
-            if(13 == e.which ) {
+            if(13 === e.which ) {
+                cell.text($('input').val());
+                $('input').remove();
+                $('tr').bind('click',addYellowClass);
             }
         });
     });
