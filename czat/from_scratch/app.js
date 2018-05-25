855244767b8fe9a0c2ec415c49a3b09e28972946
Michał Fierek <mfierek@protonmail.ch>
2018-05-25 22:18:17 +0200

    Added rooms support

--- czat/from_scratch/app.js
+++ czat/from_scratch/app.js
@@ -4,16 +4,48 @@ const serveStatic = require('serve-static');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const activeUsers = [];
+const user = {name : '', room : ''};
+
+let roomList = ['Room1'];
+
 app.use(serveStatic('public'));
 
+// set default room
+user.room = 'Room1';
+
 io.on('connection', (socket) => {
-    console.log('New user conected');
+    io.emit('setRooms', roomList);
+    socket.on('newUserConnected', (newUser) => {
+        if (activeUsers.includes(newUser.name)) {
+            socket.emit('userExist');
+        } else {
+            activeUsers.push(newUser.name);
+        }
+        console.log(newUser);
+        user.room = newUser.room;
+        user.name = newUser.name;
+        console.log(user);
+        socket.room = user.room;
+        socket.join(user.room);
+
+        io.in(user.room).emit('setRoom', user.room);
+        io.in(user.room).emit('updateUserList', activeUsers);
+        console.log('New user conected to room: ' + user.room);
+    });
+
     socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        console.log('Room: ' + user.room);
+        console.log('message: ' + user.name + ":" + msg);
+        io.in(user.room).emit('chat message', user.name + ": " + msg);
     });
 
     socket.on('disconnect', () => {
+        io.emit('updateUsersList', activeUsers);
+        activeUsers.pop(user.name);
+        user.name = '';
+        user.room = '';
+        io.emit('updateUserList', activeUsers);
         console.log('user disconected');
     });
 });
--- czat/from_scratch/public/js/script.js
+++ czat/from_scratch/public/js/script.js
@@ -4,22 +4,85 @@ function getById(id) {
 }
 document.onreadystatechange = () => {
     if (document.readyState === "interactive") {
-        let socket = io();
+        let socket;
         let form = getById('chat-form');
         let message = getById('m');
         let messages = getById('messages');
+        let username = getById('username');
+        let loginBtn = getById('login');
+        let sendBtn = getById('send');
+        let usersList = getById('active-users');
+        let roomList = getById('room-list');
+        let addRoomBtn = getById('add-room');
+        let roomInput = getById('room');
+        let defaultRoom = 'Room1';
+        let user = {name: '', room: ''};
+        
+        sendBtn.disabled = true;
+        loginBtn.disabled = false;
+
+        loginBtn.addEventListener('click', () => {
+            if (!username.value) {
+                alert("Username cannot by empty!");
+                return;
+            } else {
+                user.name = username.value;
+                console.log("Set username to: " + user.name);
+            }
+
+            if (roomInput.value) {
+                user.room = roomInput.value;
+                console.log("Set user room to: " + user.room);
+            } else {
+                user.room = "Room1";
+                console.log("Set user room to: " + user.room);
+            }
+
+            loginBtn.disabled = true;
+            sendBtn.disabled = false;
+
+            socket = io();
+            socket.emit('newUserConnected', user);
+
+            socket.on('setRooms', (rooms) => {
+                rooms.forEach( r => {
+                    let listElement = document.createElement('li');
+                    let roomButton = document.createElement('button');
+                    roomButton.innerText = r;
+                    listElement.appendChild(roomButton);
+                    roomList.appendChild(listElement); 
+                });
+            });
+
+            socket.on('chat message', (msg) => {
+                let listElement = document.createElement('li');
+                listElement.innerText = msg;
+                messages.appendChild(listElement);
+            });
+
+            socket.on('updateUserList', (activeUsers) => {
+                while(usersList.firstChild) {
+                    usersList.removeChild(usersList.firstChild);
+                }
+
+                activeUsers.forEach((activeUsers) => {
+                    let listElement = document.createElement('li');
+                    listElement.innerText = user.name;
+                    usersList.appendChild(listElement);
+                });
+            });
+
+            socket.on('userExist', () => {
+                alert('Taki użytkownik istnieje!');
+                socket.disconnect();
+            });
+        });
 
         form.addEventListener('submit', (e) => {
             e.preventDefault();
             console.log('From submitted');
             socket.emit('chat message', message.value);
             message.value = '';
-        });
-
-        socket.on('chat message', (msg) => {
-            let listElement = document.createElement('li');
-            listElement.innerText = msg;
-            messages.appendChild(listElement);
         });
     }
 };
